942018180e35c31a5183c10ff05db613fccce6a3
marvelopers <marvelopers@gmail.com>
2020-12-04 21:07:26 +0900

    add header& footer

---
+++ src/components/layout/footer/nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function footer () {
+  return (
+    <div>
+      copyright marveloper
+    </div>
+  );
+}
+
+export default nav;
---
+++ src/components/layout/header/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function nav () {
+  return (
+      <div>
+        <ul>
+          <li>js/ts</li>
+          <li>Front-end</li>
+          <li>log</li>
+        </ul>
+      </div>
+  );
+}
+
+export default nav;
