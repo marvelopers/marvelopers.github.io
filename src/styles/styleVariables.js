b90efbc419d584d492c922c49c6064e744c0483b
marvelopers <marvelopers@gmail.com>
2020-12-13 00:33:55 +0900

    Add. GlobalStyle&Color

---
+++ src/styles/styleVariables.js
@@ -0,0 +1,13 @@
+export const DARK_PURPLE = '#2B092B';
+export const PURPLE = '#4A154B';
+export const BLUE = '#1264A3';
+export const PINK = '#FF3E88';
+export const GREEN = '#2BACT6';
+export const YELLOW = '#ECB22E';
+
+export const GREY_1 = '#292929';
+export const GREY_2 = '#494949';
+export const GREY_3 = '#767676';
+export const GREY_4 = '#929292';
+export const GREY_5 = '#e8e8e8';
+export const WHITE = '#FFFFFF';
