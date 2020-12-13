7e762a39867536d59e9160409f9f7ba213ecf0a2
marvelopers <marvelopers@gmail.com>
2020-12-13 21:49:05 +0900

    ADD Color

--- src/styles/styleVariables.js
+++ src/styles/styleVariables.js
@@ -5,9 +5,13 @@ export const PINK = '#FF3E88';
 export const GREEN = '#2BACT6';
 export const YELLOW = '#ECB22E';
 
+export const DEEP_PINK = '#E01E5A';
+export const DEEP_GREEN = '#2EB67D';
+
+
 export const GREY_1 = '#292929';
 export const GREY_2 = '#494949';
 export const GREY_3 = '#767676';
 export const GREY_4 = '#929292';
-export const GREY_5 = '#e8e8e8';
+export const GREY_5 = '#E8E8E8';
 export const WHITE = '#FFFFFF';
