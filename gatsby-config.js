2b4b0803bcfe8c83155c84e6cacc901631d3ce0e
marvelopers <marvelopers@gmail.com>
2020-12-14 00:08:36 +0900

    Add V3 blog

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+const metaConfig = require('./gatsby-meta-config');
+
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-166868950-1",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    // "gatsby-plugin-manifest",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: metaConfig.title,
+        short_name: metaConfig.title,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        theme_color: `#a2466c`,
+        display: `standalone`, //`minimal-ui`
+        icon: metaConfig.icon,
+      },
+    },
+  ],
+};
