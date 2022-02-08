114dcbb64947b6daf8919fe1819a26cd02585394
marvelopers <marvelopers@gmail.com>
2022-02-08 23:51:25 +0900

    js 삭제

---
+++ src/pages/front.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from 'src/components/layout';
+import PostList from 'src/components/postList';
+import SEO from 'src/components/SEO';
+
+const FrontPage = ({ data }) => (
+  <>
+    <Layout>
+      <SEO title="Front-end" />
+      <PostList
+        posts={data.allMarkdownRemark.nodes.map((node) => ({
+          slug: node.fields.slug,
+          title: node.frontmatter.title,
+          category: node.frontmatter.category,
+          tags: node.frontmatter.tags,
+          meta: node.fields.date,
+          // meta: (
+          //   <time dateTime={node.fields.date}>{node.fields.dateStr}</time>
+          // ),
+          excerpt: node.excerpt,
+        }))}
+      />
+    </Layout>
+  </>
+);
+
+export default FrontPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      limit: 10
+      sort: { order: DESC, fields: fields___date }
+      filter: { frontmatter: { category: { eq: "front" } } }
+    ) {
+      nodes {
+        excerpt(pruneLength: 20, format: PLAIN, truncate: true)
+        fields {
+          slug
+          date(formatString: "YYYY.MM.DD")
+        }
+        frontmatter {
+          title
+          category
+          tags
+        }
+      }
+    }
+  }
+`;
