54d32262df181be3894e5c52532045926e8bac4d
marvelopers <marvelopers@gmail.com>
2022-03-30 23:15:09 +0900

    페이지 추가

---
+++ src/pages/blog/front.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { graphql, PageProps } from 'gatsby';
+import Layout from '../../components/layout/Layout';
+import PostList from '../../components/PostList';
+import { Node } from '../../models/post';
+
+type DataProps = {
+  allMdx: {
+    nodes: Node[];
+  };
+};
+
+const IndexPage = ({ data }: PageProps<DataProps>) => {
+  return (
+    <Layout>
+      <PostList posts={data.allMdx.nodes} />
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        slug
+        frontmatter {
+          category
+          date(formatString: "MMMM D, YYYY")
+          title
+          tags
+        }
+        id
+        body
+      }
+    }
+  }
+`;
