d8f5a3e25636d43b7c755201f56796a5ebdfc579
marvelopers <marvelopers@gmail.com>
2022-04-03 20:53:01 +0900

    templates 수정

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { graphql, PageProps } from 'gatsby';
+import { Frontmatter } from '../models/post';
+
+import Header from '../components/layout/Header';
+import GlobalStyle from '../styles/GlobalStyle';
+import BlogArticle from '../components/BlogArticle';
+
+export type Site = {
+  siteMetadata: {
+    title: string;
+    siteUrl: string;
+  };
+};
+
+export type Mdx = {
+  id: number;
+  body: string;
+  frontmatter: Frontmatter;
+  tableOfContents: any;
+  html: string;
+};
+
+type DataProps = {
+  site: Site;
+  mdx: Mdx;
+};
+
+const Template = ({ data }: PageProps<DataProps>) => {
+  console.log('data', data);
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <BlogArticle data={data} />
+    </>
+  );
+};
+
+export default Template;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String = "") {
+    mdx(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      body
+      excerpt(pruneLength: 160)
+      frontmatter {
+        title
+        date(formatString: "YYYY.MM.DD")
+        tags
+      }
+      fields {
+        slug
+        date
+      }
+      tableOfContents
+    }
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+  }
+`;
