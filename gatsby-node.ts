f7551b581e8804f0f317bc1779c132da5144768a
marvelopers <marvelopers@gmail.com>
2022-04-13 22:46:34 +0900

    tsconfig 설정

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,7 @@
 import { createMarkdown } from './gatsby/nodes';
 import { crateBlogPost } from './gatsby/pages';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import path from 'path';
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === 'MarkdownRemark' || node.internal.type === 'Mdx') {
@@ -9,3 +11,15 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
 exports.createPages = async ({ graphql, actions }) =>
   crateBlogPost({ graphql, actions });
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: path.resolve('tsconfig.json'),
+        }),
+      ],
+    },
+  });
+};
