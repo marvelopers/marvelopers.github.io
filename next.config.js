69e92fe617573a5fa9b4c4b3694d83120d608411
marvelopers <marvelopers@gmail.com>
2022-07-18 00:03:35 +0900

    setting next config with mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX({
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  webpack5: true,
+  webpack: (config, { defaultLoaders }) => {
+    config.resolve.fallback = { fs: false };
+
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [defaultLoaders.babel, { loader: "@mdx-js/loader" }],
+    });
+
+    return config;
+  },
+});
