4c1a78615aa7f4691e34f68f62979cbfe1586cd9
marvelopers <marvelopers@gmail.com>
2022-11-06 23:10:12 +0900

    fix: layout

--- src/components/main/Posts/styles.ts
+++ src/components/main/Posts/styles.ts
@@ -1,8 +1,24 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { SCREEN_OFFSET, MAX_SCREEN_OFFSET } from 'styles/layout';
 
 export const Posts = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  row-gap: 3.2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   padding: 6rem 0;
+
+  @media screen and (min-width: ${SCREEN_OFFSET}px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 6rem 3rem;
+  }
+
+  @media screen and (min-width: ${MAX_SCREEN_OFFSET}px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    row-gap: 3.2rem;
+  }
 `;
